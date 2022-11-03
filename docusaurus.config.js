2cffef2e07927d4786927aae780850cae5ce274c
Nahom <nahomt@amestsantim.com>
2022-11-03 12:13:48 +0300

    Changed baseUrl to /chimera-docs/

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Dashboard Starter Kit',
   tagline: 'For census and surveys, large and small',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/chimera-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
